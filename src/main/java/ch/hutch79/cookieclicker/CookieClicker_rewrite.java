1da6f0e75ea13d7871c8c23b4b5e932db345c1e5
Hutch79 <andrin.monn@gmail.com>
2023-03-16 13:48:33 +0100

    Initial commit

---
+++ src/main/java/ch/hutch79/cookieclicker/CookieClicker_rewrite.java
@@ -0,0 +1,17 @@
+package ch.hutch79.cookieclicker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CookieClicker_rewrite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
