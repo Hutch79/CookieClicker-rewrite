81de7800d28e408dbc391337153d2da748b007da
Hutch79 <andrin.monn@gmail.com>
2023-03-24 11:36:26 +0100

    Added ConfigManager, PAPI support and some initial configs. ConfigManager Source: https://www.spigotmc.org/threads/lib-file-manager-easy-config-manager.38152/

--- src/main/java/ch/hutch79/cookieclicker/CookieClicker.java
+++ src/main/java/ch/hutch79/cookieclicker/CookieClicker.java
@@ -1,12 +1,14 @@
 package ch.hutch79.cookieclicker;
 
 import ch.hutch79.cookieclicker.listener.Listener;
+import ch.hutch79.cookieclicker.util.ConfigManager;
 import ch.hutch79.cookieclicker.util.DatabaseManager;
 import com.jeff_media.updatechecker.UpdateCheckSource;
 import com.jeff_media.updatechecker.UpdateChecker;
 import com.jeff_media.updatechecker.UserAgentBuilder;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,6 +16,7 @@ public final class CookieClicker extends JavaPlugin {
 
     public final CookieClicker plugin = this;
     PluginDescriptionFile pdf = plugin.getDescription();
+    private boolean isPlaceholderApiInstalled = false;
 
     @Override
     public void onEnable() {
@@ -23,8 +26,14 @@ public final class CookieClicker extends JavaPlugin {
         final int SPIGOT_RESOURCE_ID = 105878; // Update checker
         Metrics metrics = new Metrics(this, 16433); // bStats
 
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
+        ConfigManager configManager = new ConfigManager(this);
+        configManager.getConfig("GUI.yml").copyDefaults(true).save();
+        configManager.getConfig("config.yml").copyDefaults(true).save();
+        configManager.getConfig("messages.yml").copyDefaults(true).save();
+
+        // Config noch nicht im resources ordner
+        // getConfig().options().copyDefaults();
+        // saveDefaultConfig();
 
         new UpdateChecker(this, UpdateCheckSource.SPIGET, "" + SPIGOT_RESOURCE_ID + "")
                 .setDownloadLink("https://www.spigotmc.org/resources/105878/")
@@ -54,6 +63,14 @@ public final class CookieClicker extends JavaPlugin {
         Bukkit.getConsoleSender().sendMessage("§d" + pdf.getName() + " §8> §5======================================================");
         Bukkit.getConsoleSender().sendMessage("§d" + pdf.getName() + " §8> §5| §6Database: " + DatabaseManager.isConnected());
 
+        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            Bukkit.getConsoleSender().sendMessage("§d" + pdf.getName() + " §8> §5| §aPlaceholderAPI §7has been found, hooking into it now.");
+            isPlaceholderApiInstalled = true;
+        }
+
+        Bukkit.getConsoleSender().sendMessage(configManager.getConfig("GUI.yml").get("0.item").toString());
+        Bukkit.getConsoleSender().sendMessage(configManager.getConfig("config.yml").get("database.type").toString());
+        Bukkit.getConsoleSender().sendMessage(configManager.getConfig("messages.yml").get("de.noPerm").toString());
     }
 
     @Override
@@ -74,6 +91,13 @@ public final class CookieClicker extends JavaPlugin {
 
     public CookieClicker getPlugin(){
         return plugin;
+    }
+
+    public String replacePlaceholders(Player player, String input) {
+        if(isPlaceholderApiInstalled) {
+            return me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(player, input);
+        }
+        return input;
     }
 
 }
--- src/main/java/ch/hutch79/cookieclicker/util/ConfigManager.java
+++ src/main/java/ch/hutch79/cookieclicker/util/ConfigManager.java
@@ -1,24 +1,134 @@
 package ch.hutch79.cookieclicker.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
 public class ConfigManager {
 
-    public void saveConfig(String path, String yamlPath){
-        saveConfig(path, yamlPath, false);
+    private final JavaPlugin plugin;
+    private HashMap<String, Config> configs = new HashMap<String, Config>();
+
+    public ConfigManager(JavaPlugin plugin)
+    {
+        this.plugin = plugin;
     }
 
-    public void saveConfig(String path, String yamlPath, Boolean loadDefaultOnCreate){
 
-        // Do some magic and save stuff to a config if it already exists.
-        // If not, create it and save the date after. Also, you may create it with a default config template.
+    // Get the config by the name(Don't forget the .yml)
 
+    public Config getConfig(String name) {
+        if (!configs.containsKey(name))
+            configs.put(name, new Config(name));
+
+        return configs.get(name);
     }
 
-    public String readConfig(String path, String yamlPath){
+    // Save the config by the name(Don't forget the .yml)
+    public Config saveConfig(String name) {
+        return getConfig(name).save();
+    }
 
-        // Do some magic and give me the stuff i want to know
+    // Reload the config by the name(Don't forget the .yml)
+    public Config reloadConfig(String name) {
+        return getConfig(name).reload();
+    }
 
-        return "Lol, am I really a String?";
+    public class Config {
 
+        private String name;
+        private File file;
+        private YamlConfiguration config;
+
+        public Config(String name)
+        {
+            this.name = name;
+        }
+
+        // Saves the config as long as the config isn't empty
+        public Config save() {
+            if ((this.config == null) || (this.file == null))
+                return this;
+            try
+            {
+                if (config.getConfigurationSection("").getKeys(true).size() != 0)
+                    config.save(this.file);
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+            return this;
+        }
+
+        // Gets the config as a YamlConfiguration
+        public YamlConfiguration get() {
+            if (this.config == null)
+                reload();
+
+            return this.config;
+        }
+
+        // Saves the default config(Will overwrite anything in the current config's file) Don't forget to reload after!
+        public Config saveDefaultConfig() {
+            file = new File(plugin.getDataFolder(), this.name);
+
+            plugin.saveResource(this.name, false);
+
+            return this;
+        }
+
+        // Reloads the config
+        public Config reload() {
+            if (file == null)
+                this.file = new File(plugin.getDataFolder(), this.name);
+
+            this.config = YamlConfiguration.loadConfiguration(file);
+
+            Reader defConfigStream;
+            try
+            {
+                defConfigStream = new InputStreamReader(plugin.getResource(this.name), "UTF8");
+
+                if (defConfigStream != null)
+                {
+                    YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+                    this.config.setDefaults(defConfig);
+                }
+            }
+            catch (UnsupportedEncodingException | NullPointerException e)
+            {
+                e.printStackTrace();
+            }
+            return this;
+        }
+
+
+        // Copies the config from the resources to the config's default settings.
+        // Force = true ----> Will add any new values from the default file
+        // Force = false ---> Will NOT add new values from the default file
+        public Config copyDefaults(boolean force) {
+            get().options().copyDefaults(force);
+            return this;
+        }
+
+        // An easy way to set a value into the config
+        public Config set(String key, Object value) {
+            get().set(key, value);
+            return this;
+        }
+
+        // An easy way to get a value from the config
+        public Object get(String key) {
+            return get().get(key);
+        }
     }
 
 }
